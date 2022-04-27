459402eca317ba5845f89f7555c2fcd9ec38b24b
joseantonioriosgonzalez <joseantonioriosgonzalez@localhost>
2022-04-27 13:40:52 +0200

    segundo commit con hola mundo y variable nombre.

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -1,9 +1,13 @@
 package controlador;
 
 public class Principal {
+	
+	String nombre = "Pilar";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		
 
 	}
 
